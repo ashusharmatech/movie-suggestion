3fea9a4ad085db874cc7b8952f8be6bbb505b165
Ashutosh Sharm <email2ashusharma@gmail.com>
2022-06-21 00:50:48 +0530

    initil commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
+  Button
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import axios from 'axios';
+import Movie from './Movie';
+import _ from 'lodash';
 
 function App() {
+
+  const random = (maxNumber) => {
+    return Math.floor(Math.random() * maxNumber)+1;
+  }
+
+  const API_KEY = process.env.REACT_APP_API_KEY
+
+  const TRANDING_URL = "https://api.themoviedb.org/3/trending/movie/week?api_key="+API_KEY+"&page=" + random(10);
+  const GENRE_URL = "https://api.themoviedb.org/3/genre/movie/list?api_key="+API_KEY+"&language=en-US";
+
+  const [movie, setMovie] = useState();
+  const [genreMap, setGenreMap] = useState();
+  const getMovie = () => {
+    axios.get(TRANDING_URL)
+      .then(res => {
+        console.log(res.data);
+        let movieList = res.data.results;
+        let m = movieList[random(movieList.length)];
+        console.log(m);
+        setMovie(m);
+      })
+  }
+
+  const getGenre = (value) => {
+    let genres = _.filter(genreMap, ({id}) => id === value);
+    return genres[0].name
+  }
+
+
+  useEffect(() => {
+    axios.get(GENRE_URL)
+      .then(res => {
+        setGenreMap(res.data.genres);
+      })
+  }, [])
+
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Button colorScheme={'blue'} size={'sm'} onClick={() => getMovie()}>
+              What to watch ?
+            </Button>
           </VStack>
+          {movie && <Movie data={movie} getGenre={getGenre}></Movie>}
         </Grid>
       </Box>
     </ChakraProvider>
