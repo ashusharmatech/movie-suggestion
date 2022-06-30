09ef1bbb1aea97a887b837aebc58fb7106a8ec50
Ashutosh Sharm <email2ashusharma@gmail.com>
2022-06-30 18:42:50 +0530

    route correction

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <>
     <ColorModeScript />
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </>
