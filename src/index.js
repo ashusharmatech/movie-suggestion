392a7ff4ce88a6848416dd693f4e445baf3a273f
Ashutosh Sharm <email2ashusharma@gmail.com>
2022-06-30 17:52:35 +0530

    hashrouter changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <>
     <ColorModeScript />
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </Router>
   </>
 );
