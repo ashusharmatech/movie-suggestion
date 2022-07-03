5322abf313ee3d4f4965b5a0b3dc752a395baa8c
Ashutosh Sharm <email2ashusharma@gmail.com>
2022-07-03 23:05:01 +0530

    Menubar changes

--- src/constant/Menu.js
+++ src/constant/Menu.js
@@ -1,20 +1,26 @@
 
 export const NAV_ITEMS = [
+
 	{
-		label: 'Popular',
-		href: '/popular'
-	},
-	{
-		label: 'Top Rated',
-		href: '/top'
-	},
-	{
-		label: 'Upcoming',
-		href: '/upcoming'
-	},
-	{
-		label: 'Now Playing',
-		href: '/now_playing'
+		label: 'Movies',
+		children: [
+
+			{
+				label: 'Popular',
+				href: '/popular'
+			},
+			{
+				label: 'Top Rated',
+				href: '/top'
+			},
+			{
+				label: 'Upcoming',
+				href: '/upcoming'
+			},
+			{
+				label: 'Now Playing',
+				href: '/now_playing'
+			}]
 	},
 	{
 		label: 'Random',
